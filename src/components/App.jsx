a2d4cc5e8426fbc6b12709428006d8257b1420d4
tkulagina <tkulagina1974@gmail.com>
2023-10-22 11:26:34 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,108 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as API from '../services/Api';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Modal from './Modal/Modal';
+import Loader from './Loader/Loader';
+import { AppDiv } from './App.styled';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import {Searchbar} from './Searchbar/Searchbar';
-import {ImageGallery} from './ImageGallery/ImageGallery';
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isLastPage, setIsLastPage] = useState(false);
 
-import fetchImagesWithQuery from 'services/api';
-
-import {Modal} from './Modal/Modal';
-import {Loader} from './Loader/Loader';
-import {Button} from './Button/Button';
-import css from './App.module.css';
-
-
-export const App = () => {
-  const [searchData, setSearchData] = useState ('');
-  const [images, setImages] = useState ([]);
-  const [page, setPage] = useState (0);
-  const [largeImage, setLargeImage] = useState ('');
-  const [showModal, setShowModal] = useState (false);
-  const [isLoading, setIsLoading] = useState (false);
-  //eslint-disable-next-line
-  const [error, setError] = useState ('');
-  
   useEffect(() => {
-    if (!page) {
+    if (!query) {
+      return;
+    }
+    const fetchImages = async () => {
+      try {
+        setIsLoading(true);
+        const data = await API.getImages(query, page);
+
+        if (data.hits.length === 0) {
+          toast.error('Sorry, there are no images matching your request...');
+          return;
+        }
+
+        const normalizedImages = API.normalizedImages(data.hits);
+        setIsLoading(false);
+        setImages(prevImages => [...prevImages, ...normalizedImages]);
+        setIsLastPage(page >= Math.ceil(data.totalHits / 12));
+        setError(null);
+      } catch (error) {
+        setError(error.message);
+        toast.error('Sorry, something went wrong.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (query !== '') {
+      fetchImages();
+    }
+  }, [query, page]);
+
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const handleSearchSubmit = newQuery => {
+    if (query === newQuery) {
       return;
     }
 
-    try {
-      setIsLoading(true);
-      const response = fetchImagesWithQuery(searchData, page);
-      response.then(data => {
-        data.data.hits.length === 0
-          ? toast.error('Nothing found')
-          : data.data.hits.forEach(({ id, webformatURL, largeImageURL }) => {
-              !images.some(image => image.id === id) &&
-                setImages(i => [...i, { id, webformatURL, largeImageURL }]);
-            });
-        setIsLoading(false);
-      });
-    } catch (error) {
-      setError(error);
-      setIsLoading(false);
-    } finally {setIsLoading(false)};
-   
-  }, [images, page, searchData]);
-
-const onSubmit = searchData => {
-  setSearchData(searchData);
-  setPage(1);
-  setImages([]);
-}
-/*const onSubmit = newSearchData => {
-  console.log (newSearchData);
-  if (newSearchData.trim() === '') {
-    return toast.error('Enter the meaning for search');
-  } else if (newSearchData === searchData) {
-    return;
-  }
-  setSearchData(newSearchData);
-  setPage(1);
-  setImages([]);*/
-
-
-  const nextPage = () => {
-    setPage(prevState => prevState + 1);
+    setQuery(newQuery);
+    setPage(1);
+    setImages([]);
+    setError(null);
+    setIsLastPage(false);
   };
 
-  const openModal = index => {
-    setShowModal (true);
-    setLargeImage (images[index].largeImageURL);    
+  const handleImageClick = image => {
+    setSelectedImage(image);
+    setShowModal(true);
   };
 
-  const toggleModal = () => {
-    setShowModal (!showModal);
+  const handleModalClose = () => {
+    setSelectedImage(null);
+    setShowModal(false);
   };
+  return (
+    <AppDiv>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={true}
+      />
+      <Searchbar onSubmit={handleSearchSubmit} />
 
-     return (
+      {error && <p>Error: {error}</p>}
+
+      <ImageGallery images={images} onItemClick={handleImageClick} />
+
+      {isLoading && <Loader />}
+
+      {!isLoading && images.length > 0 && !isLastPage && (
+        <Button onClick={loadMore} />
+      )}
+
+      {showModal && <Modal image={selectedImage} onClose={handleModalClose} />}
+    </AppDiv>
+  );
+};
+
+export default App;
+
+    /* return (
       <div className={css.app}>
         <Searchbar onSubmit={onSubmit} />
         {images.length !== 0 && (
@@ -93,4 +118,4 @@ const onSubmit = searchData => {
                 
       </div>
     );  
-}
+}*/
